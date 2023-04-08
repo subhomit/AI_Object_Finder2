61ad1401763f0cddcb3f3865ecb338fa4d30d76b
subhomit <88931246+subhomit@users.noreply.github.com>
2023-04-08 15:28:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+status = "";
+input_text = "";
+
+function setup(){
+    canvas = createCanvas(300,290);
+    canvas.position(480,250);
+    video = createCapture(VIDEO);
+    video.size(300,290);
+    video.hide();
+}
+function start(){
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+    input_text = document.getElementById("input_id").value;
+}
+function modelLoaded(){
+    console.log("Model_Loaded");
+    status = true;
+}
+function draw(){
+    image(video,0,0,300,290);
+}
